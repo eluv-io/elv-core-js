66fb9f2e16a67ad1539ccd3d82daa13706c7c562
Kevin Talmadge <kevin@eluv.io>
2019-07-19 12:36:22 -0700

    Update dist

--- dist/configuration.js
+++ dist/configuration.js
@@ -1,9 +1,7 @@
 const EluvioConfiguration = {
-  "config-url": "https://main.net955304.contentfabric.io/config",
+  "config-url": "https://main.net955210.contentfabric.io/config",
   "apps": {
     "fabric-browser": "http://localhost:8080",
     "Eluvio Video Editor": "http://localhost:8083"
   }
 };
-
-module.exports = EluvioConfiguration;
