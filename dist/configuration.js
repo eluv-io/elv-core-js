77cce423dd329fe0901688eb11224334a55441cf
Kevin Talmadge <kevin@eluv.io>
2020-02-11 11:23:52 -0800

    Update elv-client-js

--- dist/configuration.js
+++ dist/configuration.js
@@ -1,6 +1,6 @@
 const EluvioConfiguration = {
-  "config-url": "https://main.net955210.contentfabric.io/config",
-  //"config-url": "https://main.net955305.contentfabric.io/config",
+  //"config-url": "https://main.net955210.contentfabric.io/config",
+  "config-url": "https://main.net955305.contentfabric.io/config",
   //"config-url": "https://demov3.net955210.contentfabric.io/config",
   "apps": {
     "Eluvio Fabric Browser": "http://localhost:8080",
