f744c8ad04ea592c941a794bd7b4c09654357681
Kevin Talmadge <kevin@eluv.io>
2020-06-22 10:57:50 -0700

    Add active/inactive title note to offering page

--- src/components/Offerings.js
+++ src/components/Offerings.js
@@ -8,6 +8,12 @@ class Offerings extends React.Component {
     return (
       <div className="offerings-container">
         { OfferingsTable }
+        <div className="note">
+          * An Active Title is a primary title content object that receives media playback or download requests (other than listing public metdata) and/or is updated at least once within the billing month.
+        </div>
+        <div className="note">
+          * An Inactive Title is a primary title content object that does not receive any media playback or download requests (other than listing public metadata) and is not updated for the duration of the billing month.
+        </div>
       </div>
     );
   }
