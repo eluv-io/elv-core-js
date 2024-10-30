aab5ab21e305229ab1ba190f64310c69a34dc897
Victor Amigo <victor.amigo@eluv.io>
2024-10-30 11:45:27 -0700

    syncing changes

--- configuration-example.js
+++ configuration-example.js
@@ -1,8 +1,8 @@
 const EluvioConfiguration = {
   // Fabric Network
-  "config-url": "https://test.net955203.contentfabric.io/config",
+  // "config-url": "https://test.net955203.contentfabric.io/config",
   // "config-url": "https://main.net955305.contentfabric.io/config",
-  // "config-url": "https://demov3.net955210.contentfabric.io/config",
+  "config-url": "https://demov3.net955210.contentfabric.io/config",
 
   //v4
   // "config-url": "https://host-468.contentfabric.io/config",
